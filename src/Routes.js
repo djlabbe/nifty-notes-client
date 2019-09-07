ece4c22ece9e685834be87d962cb3a74fab19dbc
Douglas Labbe <dougjlabbe@gmail.com>
2019-09-06 22:03:03 -0700

    signup flow

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute"
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
 
@@ -10,5 +11,6 @@ export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
         <AppliedRoute path="/login" exact component={Login} props={childProps} />
+        <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
         <Route component={ NotFound } />
     </Switch>
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -34,6 +34,7 @@ export default class Login extends Component {
       this.props.history.push("/");
     } catch (e) {
       alert(e.message);
+      this.setState({ isLoading: false });
     }
   };
 
